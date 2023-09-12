5a4f901b301e1f7f22253cc65f884521ac38c9df
Ezedike Evan <120946193+Ezedike-Evan@users.noreply.github.com>
2023-09-12 14:22:53 +0000

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const URL = 'mongodb+srv://E4:ZjuMW8UpWKrjw44M@cluster0.jqsjmw3.mongodb.net/?retryWrites=true&w=majority'
 
 async function dbConnection(){
-    await  mongoose.connect(URL)
+await  mongoose.connect(URL)
     try {
         app.listen(port, () => console.log(`Server is running on port ${port}`))        
     } catch (error) {
